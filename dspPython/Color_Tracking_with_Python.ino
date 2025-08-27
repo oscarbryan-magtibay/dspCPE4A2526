5a80bae9a811538c8a7f71bfd3fcbddd5d2060da
Dominic-Ramos <1920265@ub.edu.ph>
2025-08-27 13:20:46 +0800

    Create Color_Tracking_with_Python.ino

---
+++ dspPython/Color_Tracking_with_Python.ino
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+static const int SERVO_PIN = 18;    // SERVO MOTOR PIN 
+static const int BAUD = 115200;     
+static const int ANGLE_MIN = 0;
+static const int ANGLE_MAX = 180;
+static const int START_ANGLE = 90;
+
+Servo servo;
+String line;
+
+void safeWrite(int angle) {
+  angle = constrain(angle, ANGLE_MIN, ANGLE_MAX);
+  servo.write(angle);
+}
+
+void setup() {
+  Serial.begin(BAUD);
+  delay(200);
+
+  servo.setPeriodHertz(50);
+  servo.attach(SERVO_PIN, 500, 2400); 
+  safeWrite(START_ANGLE);
+
+  Serial.println("ESP32 ready");
+}
+
+void loop() {
+  while (Serial.available()) {
+    char c = (char)Serial.read();
+    if (c == '\r') continue; 
+    if (c == '\n') {
+      if (line.startsWith("A:")) {
+        int angle = line.substring(2).toInt();
+        safeWrite(angle);
+        Serial.print("ACK:");
+        Serial.println(angle);
+      }
+      line = "";
+    } else {
+      if (line.length() < 16) line += c;
+    }
+  }
+}
