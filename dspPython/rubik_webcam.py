ca24000b70adecf4c2ab2dc93446faa3db2b0bad
ChrisnelSilva <2420636@ub.edu.ph>
2025-09-19 11:39:52 +0800

    Updated Activity - Sep 9, 2025
    Activity 8 and Rubik webcam

--- dspPython/rubik_webcam.py
+++ dspPython/rubik_webcam.py
@@ -5,34 +5,34 @@ from ultralytics import YOLO
 import numpy as np
 import matplotlib.pyplot as plt
 
-# ✅ Load your trained YOLO model
+
 model = YOLO(r"C:\Users\Administrator\Documents\GitHub\dspCPE4A2526\best2.pt")
 
-# ✅ Open webcam
+#  Open webcam
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
-    print("❌ Could not open webcam.")
+    print("Could not open webcam.")
 else:
-    print("✅ Webcam opened. Press 'q' to quit.")
+    print("Webcam opened. Press 'q' to quit.")
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("❌ Failed to grab frame.")
+        print("Failed to grab frame.")
         break
 
-    # ✅ Run YOLO detection
+    #  Run YOLO detection
     results = model(frame, conf=0.8)
     annotated_frame = results[0].plot()
 
-    # ✅ Show in external OpenCV window
+    #  Show in external OpenCV window
     cv2.imshow("Rubik Detection - Live", annotated_frame)
 
     # Exit when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# ✅ Cleanup
+
 cap.release()
 cv2.destroyAllWindows()
